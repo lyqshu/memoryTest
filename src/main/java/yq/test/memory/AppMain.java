3ee8118e75b81698788bd835378308a21a611e12
Yingqiao Liu <yingq.liu@sap.com>
2017-02-24 18:47:53 +0800

    mem leak

--- src/main/java/yq/test/memory/AppMain.java
+++ src/main/java/yq/test/memory/AppMain.java
@@ -3,15 +3,22 @@ package yq.test.memory;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 /**
  * Created by I030518 on 2017-02-18.
  */
 public class AppMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         List<LeakNode> ls = new ArrayList<LeakNode>();
+        long i=0;
         while(1==1){
-            //ls.add(new LeakNode());
-
+            i++;
+            LeakNode leakNode = new LeakNode();
+            if(i%10==0){
+                ls.add(leakNode);
+            }
+            sleep(20);
         }
     }
 }
